cdcf16696e9cd0749f75f3b44e74c06fe922ea0f
enuoCM <enuoCM@163.com>
2024-09-24 11:29:53 +0800

    support query params for command

--- src/api/src/http/command.rs
+++ src/api/src/http/command.rs
@@ -7,6 +7,7 @@ use crate::session::SessionMap;
 /// GET /cfs/{addr}/commands/commands
 pub(crate) async fn get_commands(
     request: HttpRequest,
+    data: web::Bytes,
     _map: web::Data<SessionMap<'_>>,
 ) -> crate::http::Result<HttpResponse> {
     info!("confilesystem - get_commands(): request = {:?}", request);
@@ -17,10 +18,15 @@ pub(crate) async fn get_commands(
         .ok_or_else(|| Error::InvalidRequest(String::from("no `addr` in url")))?
         .to_string().to_lowercase();
     info!("confilesystem - get_commands(): addr = {:?}", addr);
+    let data_vec = data.as_ref().to_vec().clone();
+    let extra_request = std::str::from_utf8(&data_vec).map_err(|e| {
+        Error::InvalidRequest(format!("illegal extra request: {e}"))
+    })?;
+    info!("confilesystem - get_commands(): extra_request = {:?}", extra_request.clone());
 
     let cfsi = cfs::Cfs::new("".to_string(), "".to_string())
         .map_err(|e| Error::GetCommandsFailed(format!("new cfs error: {e}")))?;
-    let get_rsp = cfsi.get_resource(addr.clone(), "commands".to_string(), "commands".to_string(), "".as_ref())
+    let get_rsp = cfsi.get_resource(addr.clone(), "commands".to_string(), "commands".to_string(), extra_request)
         .await
         .map_err(|e| Error::GetCommandsFailed(format!("get commands error: {e}")))?;
     info!("confilesystem - get_commands(): get_rsp = {:?}", get_rsp);
