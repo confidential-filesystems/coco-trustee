b7ad8fcdce3d6c457dcdc969be03bd095d01b40d
enuoCM <enuoCM@163.com>
2024-09-29 15:20:50 +0800

    update build version to r1

--- build_trustee.sh
+++ build_trustee.sh
@@ -3,7 +3,7 @@
 set -e
 SERVICE_NAME=coco-trustee
 HUB=hub.confidentialfilesystems.com:30443
-VERSION=${1:-v0.8.0-filesystem-d9}
+VERSION=${1:-v0.8.0-filesystem-r1}
 SSH_KEY=${2:-$HOME/.ssh/id_rsa}
 
 docker build --ssh default=${SSH_KEY} -f ./coco-trustee.dockerfile -t ${HUB}/cc/${SERVICE_NAME}:${VERSION} .
